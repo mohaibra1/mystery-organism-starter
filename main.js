36636713852282872c9085da72a4fbbb456fec61
mohaibra <mimohadinho@gmail.com>
2023-08-26 03:42:53 +0200

    completed

--- main.js
+++ main.js
@@ -13,6 +13,67 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+const pAequorFactory = (num, DnaBases) => {
+  return {
+    _specimenNum: num,
+    _dna: DnaBases,
+    get specimenNum(){
+      return this._specimenNum;
+    },
+    get dna(){
+      return this._dna;
+    },
+    mutate(){
+      let firstElement = this._dna[0];
+      let checkDna = '';
+      let notFirstElement = false;
+      // do {
+      //    checkDna = returnRandBase();
+         
+      // }
+      while(!notFirstElement){
+        checkDna = returnRandBase();
+          if(firstElement !== checkDna){
+            this._dna[0] = checkDna;
+            notFirstElement = true;
+          }
+      }
+      return this._dna;
+    },
+    compareDNA(pAequor){
+        let countIdenticalDNA = 0;
+        let indenticalDNA = [];
+        //pAequor.dna.filter(s => !this._dna.includes(s)? indenticalDNA.push(s): '');
+        for(let pDNA of pAequor.dna){
+          if(pDNA === 'T'){
+           if(this._dna.includes(pDNA)){
+            if(!indenticalDNA.includes(pDNA)){
+              indenticalDNA.push(pDNA);
+            }
+           }
+          }
+        }
+       let percentage = Math.floor((indenticalDNA.length/4) *100);
+         
+       console.log('specimen ' + pAequor.specimenNum + ' and specimen ' + this._specimenNum + ' have ' + percentage + '% DNA in common.');
+    },
+    willLikelySurvive(){
+     
+    }
+  }
+}
+
+const mock = mockUpStrand();
+console.log(mock);
+
+const obj = pAequorFactory(2, mock);
+
+console.log(obj.mutate());
+const mock2 = mockUpStrand();
+const mock3 = mockUpStrand();
+const obj2 = pAequorFactory(5, mock3);
+console.log(obj.compareDNA(obj2));
+
 
 
 
